de3cba6d4343adfe60c7af0f79d005261c4c2563
azandrew-sidoine <asmyns.platonnas29@gmail.com>
2020-04-26 00:00:40 +0000

    removed test file and css file on contribution sidebar

--- contributions-default-sidebar/contributions-default-sidebar.component.ts
+++ contributions-default-sidebar/contributions-default-sidebar.component.ts
@@ -5,7 +5,7 @@ import { partialConfigs, defaultPath, contributionPath, RC_STRUCTURES, RC_MEMBER
 @Component({
   selector: 'app-contributions-default-sidebar',
   templateUrl: './contributions-default-sidebar.component.html',
-  styleUrls: ['./contributions-default-sidebar.component.scss']
+  styles: []
 })
 
 export class ContributionsDefaultSidebarComponent implements OnInit {
@@ -18,12 +18,12 @@ export class ContributionsDefaultSidebarComponent implements OnInit {
 
   ngOnInit() {
     this.navbarRouteDefinitions = {
-      navbar_tiei: 'Gestion TIEI',
+      navbar_tiei: 'Gestion des cotisations (RCAP)',
       navbar_tiei_enregistrement: 'Enregistrement des cotisations',
       navbar_tiei_cotisation: 'Gestion des cotisations',
       navbar_manage_individual_accounts: 'Comptes individuels',
       navbar_manage_accounts: 'Comptes cotisants',
-      navbar_rc: 'Gestion RC',
+      navbar_rc: 'Gestion des cotisations  (RCOMP)',
       navbar_rc_enregistrement: 'Enregistrement des cotisations',
       navbar_rc_contribution_declarations: 'Gestion des cotisations (Assurés)',
       navbar_rc_structure_contribution_declarations: 'Gestion des cotisations (Employeurs / Faitières)',
