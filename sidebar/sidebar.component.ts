755d82062291f3564a9765509e78fa59b6127fca
azandrew-sidoine <asmyns.platonnas29@gmail.com>
2021-02-23 18:44:09 +0000

    Applied styles to content containers

--- sidebar/sidebar.component.ts
+++ sidebar/sidebar.component.ts
@@ -1,43 +1,56 @@
 import { Component, OnInit, Input } from '@angular/core';
 import { RouteLink, RoutesMap, builLinkFromRoutesMap, IRouteLinkCollectionItem } from 'src/app/lib/domain/routes';
-import { AbstractAlertableComponent } from 'src/app/lib/domain/helpers/component-interfaces';
-import { AppUIStoreManager } from 'src/app/lib/domain/helpers/app-ui-store-manager.service';
 import { AuthService } from 'src/app/lib/domain/auth/core';
 import { Collection } from 'src/app/lib/domain/collections';
 import { TypeUtilHelper } from '../../../domain/helpers/type-utils-helper';
 import { Authorizable, userCanAny } from 'src/app/lib/domain/auth/contracts/v2';
 import { map } from 'rxjs/operators';
+import { createStateful } from '../../../domain/rxjs/helpers/creator-functions';
+import { combineLatest } from 'rxjs';
+import { ICollection } from 'src/app/lib/domain/contracts/collection-interface';
 
 @Component({
   selector: 'app-sidebar',
   templateUrl: './sidebar.component.html',
   styles: []
 })
-export class SidebarComponent extends AbstractAlertableComponent implements OnInit {
-  public navigationRoutes: Collection<RouteLink>;
+export class SidebarComponent implements OnInit {
+  // public navigationRoutes: Collection<RouteLink>;
   public routesIndexes: string[];
-  @Input() routesMap: RoutesMap[];
+  private _routesMap: RoutesMap[] = [];
   @Input() routeDescriptions: { [index: string]: string };
+  @Input() set routesMap(value: RoutesMap[]) {
+      this._routesMap = value;
+  }
+  get routesMap() {
+    return this._routesMap;
+  }
+  // @Input() expanded = true;
 
-  state$ = this.auth.state$.pipe(
-    map(state => state.user)
+  private _navigationRoutes$ = createStateful<Collection<RouteLink>>(new Collection());
+  state$ = combineLatest([
+    this.auth.state$,
+    this._navigationRoutes$.asObservable()
+  ]).pipe(
+    map(([state, routes]) => ({
+      user: state.user,
+      routes
+    })),
   );
 
   constructor(
-    public appUIStoreManager: AppUIStoreManager,
     public auth: AuthService,
     public readonly typeHelper: TypeUtilHelper
-  ) {
-    super(appUIStoreManager);
-    this.navigationRoutes = new Collection();
-  }
+  ) {}
 
   ngOnInit(): void {
-    this.routesIndexes = this.routesMap.map((route) => route.key);
-    builLinkFromRoutesMap(this.routesMap, this.routeDescriptions).forEach(
+    const collection = new Collection<RouteLink>();
+    this.routesIndexes = this._routesMap.map((route) => route.key);
+    builLinkFromRoutesMap(this._routesMap, this.routeDescriptions).forEach(
       (item: IRouteLinkCollectionItem) =>
-        this.navigationRoutes.add(item.key, item.value)
+        collection.add(item.key, item.value)
     );
+    this._navigationRoutes$.next(collection);
   }
 
   isFirstRoute(routes: RoutesMap[], item: RoutesMap): boolean {
@@ -48,8 +61,8 @@ export class SidebarComponent extends AbstractAlertableComponent implements OnIn
    * @description Get [[RouteLink]] instance from the collection of RouteLink
    * @param key [[string]]
    */
-  public getRouteLinkFromMap(key: string): RouteLink {
-    return this.navigationRoutes.get(key);
+  public getRouteLinkFromMap(routes: ICollection<RouteLink>, key: string): RouteLink {
+    return routes.get(key);
   }
 
   hasAuthorizations(user: Authorizable, authorizations: string[]): boolean {
