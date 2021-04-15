2f410083c3f3715f3e68bf7cbb87a0f17c61ea34
adevou <ceo@coucoumail.com>
2021-04-15 06:15:16 +0000

    AddedRoutes for current project

--- admin-route-definitions.ts
+++ admin-route-definitions.ts
@@ -1,11 +1,30 @@
 export const routeDefinitions = {
+  // --- APP USER ROUTE -----
+  // Données entrantes
+  incomingListRoute: 'incoming/list',
+  incomingEditRoute: 'incoming/edit',
 
-  right_list: 'right_list',
-  finger: 'finger',
-  session: 'session',
-  device: 'device',
-  recipient: 'recipient',
-  
+  // Gestion des données de controle
+  controlListRoute: 'control/list', // liste des controles
+  controlStatsRoute: 'control/stats', // Statistiques des controle
+
+  // Gestion des droits et Beneficiaires
+  rightsListRoute: 'rights/list',
+  recipientsRoute: 'rights/recipients',
+  recipientsInactiveRoute: 'rights/inactive',
+  recipientsActiveRoute: 'rights/active',
+
+  // Réglages de controles
+  settingsRoute: 'settings',
+  movingDataRoute: 'settings/migrate-data', //Import & Export
+  rightsTypeRoute: 'settings/rights', // Type of rights
+  devicesRoute: 'settings/devices', //Liste des peripheriques de controle
+  editionsRoute: 'settings/editions', // Editions de controle de vie
+  sessionsRoute: 'settings/sessions', // Liste des sessions de controle par edition
+  dataTypesRoute: 'settings/data-type', // Type de donnees biometriques
+
+  // ----- ADMIN ROUTES -----
+
   // Management routes
   managementsRoute: 'managements',
   // Account routes
