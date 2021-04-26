fc0915805b67e98eb4ea6c5bb2a974afbf88b622
Adevou Fera EKPEH <adevfera@imac.local>
2021-04-26 19:54:08 +0000

    Created client routes

---
+++ controlvie-client-route-definitions.ts
@@ -0,0 +1,23 @@
+export const routeDefinitions = {
+  // Enrolement
+  enrollDashboardRoute: 'enroll/dashboard',
+  enrollDemographicRoute: 'enroll/demographic',
+  enrollUserIdCardRoute: 'enroll/useridcard',
+  enrollUserFacialRoute: 'enroll/userFacial',
+  enrollUserVoiceRoute: 'enroll/uservoice',
+  enrollUserFingerPrintsRoute: 'enroll/userfingerprint',
+  enrollUserReceiptRoute: 'enroll/userreceipt',
+
+  // Controle de Vie
+  controlStartRoute: 'control/start', 
+  controlPhoneCheckRoute: 'control/phonecheck', 
+  controlDashboardRoute: 'control/dashboard',
+  controlUserFacialRoute: 'control/userFacial',
+  controlUserVoiceRoute: 'control/uservoice',
+  controlUserFingerPrintsRoute: 'control/userfingerprint', 
+  controlUserReceiptRoute: 'control/userreceipt',
+
+  // Réglages de controles
+  userProfileRoute: 'settings/userprofile',
+  userChangePassRoute: 'settings/changepassword', 
+};
