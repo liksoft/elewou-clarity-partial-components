0b0bc6788107df0fc74b6a7e4c9930bd85ea0148
azandrew-sidoine <asmyns.platonnas29@gmail.com>
2022-02-06 15:25:43 +0000

    Rewrite parse-date pipes to use JSDate instead of MomentUtils

--- pipes/parse-date.pipe.ts
+++ pipes/parse-date.pipe.ts
@@ -1,50 +1,54 @@
-import { Pipe, PipeTransform } from '@angular/core';
-import * as moment from 'moment';
-import 'moment/locale/fr';
-import { MonthProvider } from '../../../core/utils/datetime/months';
-import { isDefined } from '../../../core/utils/types/type-utils';
+import { Inject, Optional, Pipe, PipeTransform } from "@angular/core";
+import { NAVIGATOR } from "src/app/lib/core/utils/ng/common";
+import { isDefined, JSDate, MonthProvider, TimeAgo } from "../../../core/utils";
 
 @Pipe({
-  name: 'parseDate'
+  name: "parseDate",
 })
 export class ParseDatePipe implements PipeTransform {
-
   transform(value: any, args?: any): any {
-    return !isDefined(value) ? '' : (moment.isDate(new Date(value)) ?  moment(value).format(args ? args : 'DD/MM/YYYY') : value);
+    return !isDefined(value)
+      ? ""
+      : JSDate.isDate(value)
+      ? JSDate.format(value, args ? args : "DD/MM/YYYY")
+      : value;
   }
-
 }
 
 @Pipe({
-  name: 'dateTime'
+  name: "dateTime",
 })
 export class DateTimePipe implements PipeTransform {
-
   transform(value: any, args?: any): any {
-    return !isDefined(value) ? '' : (moment.isDate(new Date(value)) ?  moment(value).format(args ? args : 'lll') : value);
+    return !isDefined(value)
+      ? ""
+      : JSDate.isDate(value)
+      ? JSDate.format(value, args ? args : "lll")
+      : value;
   }
-
 }
 
 @Pipe({
-  name: 'timeago'
+  name: "timeago",
 })
 export class TimeAgoPipe implements PipeTransform {
+  // Instance initializer
+  constructor(@Inject(NAVIGATOR) @Optional() private navigator?: Navigator) {}
 
-  transform(value: moment.MomentInput): any {
-    return !isDefined(value) ? '' : (moment.isDate(value) ?  moment(value).fromNow() : value);
+  transform(value: any, locale: string = "fr-FR"): string {
+    return !isDefined(value)
+      ? ""
+      : JSDate.isDate(value)
+      ? new TimeAgo().format(JSDate.create(value), locale ?? this.navigator?.language)
+      : value;
   }
-
 }
 
-
 @Pipe({
-  name: 'parseMonth'
+  name: "parseMonth",
 })
 export class ParseMonthPipe implements PipeTransform {
-
   transform(value: any): any {
-    return !isDefined(value) ? '' : MonthProvider.parseMonth(value);
+    return !isDefined(value) ? "" : MonthProvider.parseMonth(value);
   }
-
 }
