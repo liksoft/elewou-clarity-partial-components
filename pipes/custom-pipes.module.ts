4bf1dfd9d50f76c1c181d08a9cc62ebd4eb9979c
azandrew-sidoine <asmyns.platonnas29@gmail.com>
2022-02-21 20:12:28 +0000

    Removed amountFormatter pipe

--- pipes/custom-pipes.module.ts
+++ pipes/custom-pipes.module.ts
@@ -1,9 +1,22 @@
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { AmountFormaterPipe, FormatAmountPipe, ParseInt, PositiveNumber } from './numbers-formats.pipe';
-import { ParseDatePipe, TimeAgoPipe, ParseMonthPipe, DateTimePipe } from './parse-date.pipe';
-import { SafeWebContentPipe, SafeRessourceContentPipe, CheckScriptsPipe } from './safe-web-content.pipe';
-import { MaskedContentPipe } from './masked-content.pipe';
+import { NgModule } from "@angular/core";
+import { CommonModule } from "@angular/common";
+import {
+  FormatAmountPipe,
+  ParseInt,
+  PositiveNumber,
+} from "./numbers-formats.pipe";
+import {
+  ParseDatePipe,
+  TimeAgoPipe,
+  ParseMonthPipe,
+  DateTimePipe,
+} from "./parse-date.pipe";
+import {
+  SafeWebContentPipe,
+  SafeRessourceContentPipe,
+  CheckScriptsPipe,
+} from "./safe-web-content.pipe";
+import { MaskedContentPipe } from "./masked-content.pipe";
 
 @NgModule({
   imports: [CommonModule],
@@ -17,9 +30,8 @@ import { MaskedContentPipe } from './masked-content.pipe';
     SafeRessourceContentPipe,
     CheckScriptsPipe,
     DateTimePipe,
-    AmountFormaterPipe,
     ParseInt,
-    MaskedContentPipe
+    MaskedContentPipe,
   ],
   exports: [
     FormatAmountPipe,
@@ -31,9 +43,8 @@ import { MaskedContentPipe } from './masked-content.pipe';
     SafeRessourceContentPipe,
     CheckScriptsPipe,
     DateTimePipe,
-    AmountFormaterPipe,
     ParseInt,
-    MaskedContentPipe
-  ]
+    MaskedContentPipe,
+  ],
 })
 export class PipesModule {}
