df34884596650c7c9bbf3080367a2b37a7165852
azandrew-sidoine <asmyns.platonnas29@gmail.com>
2022-03-21 15:45:57 +0000

    Fixed CustomPipes errors

--- pipes/custom-pipes.module.ts
+++ pipes/custom-pipes.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { AmountFormaterPipe, FormatAmountPipe, ParseInt, PositiveNumber } from './numbers-formats.pipe';
+import { FormatAmountPipe, ParseInt, PositiveNumber } from './numbers-formats.pipe';
 import { ParseDatePipe, TimeAgoPipe, ParseMonthPipe, DateTimePipe } from './parse-date.pipe';
 import { SafeWebContentPipe, SafeRessourceContentPipe, CheckScriptsPipe } from './safe-web-content.pipe';
 import { MaskedContentPipe } from './masked-content.pipe';
@@ -17,7 +17,6 @@ import { MaskedContentPipe } from './masked-content.pipe';
     SafeRessourceContentPipe,
     CheckScriptsPipe,
     DateTimePipe,
-    AmountFormaterPipe,
     ParseInt,
     MaskedContentPipe
   ],
@@ -31,7 +30,6 @@ import { MaskedContentPipe } from './masked-content.pipe';
     SafeRessourceContentPipe,
     CheckScriptsPipe,
     DateTimePipe,
-    AmountFormaterPipe,
     ParseInt,
     MaskedContentPipe
   ]
