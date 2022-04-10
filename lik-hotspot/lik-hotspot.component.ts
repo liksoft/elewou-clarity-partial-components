78a6cb75e014febe64f1d70362470121dbf14d5c
azandrew-sidoine <asmyns.platonnas29@gmail.com>
2022-04-10 19:38:38 +0000

    Update likhostpot component information icon button styles Add toggling implementation on information button

--- lik-hotspot/lik-hotspot.component.ts
+++ lik-hotspot/lik-hotspot.component.ts
@@ -1,19 +1,27 @@
-import { Component, OnInit, ChangeDetectionStrategy, Input } from '@angular/core';
+import {
+  Component,
+  OnInit,
+  ChangeDetectionStrategy,
+  Input,
+} from '@angular/core';
 
 @Component({
   selector: 'app-lik-hotspot',
   templateUrl: './lik-hotspot.component.html',
   styleUrls: ['./lik-hotspot.component.scss'],
-  changeDetection: ChangeDetectionStrategy.OnPush
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class LikHotspotComponent implements OnInit {
-
-  constructor() { }
-  @Input() public message = "This is a message";
-  @Input() public classes = "relative-top";
+  @Input() public message = 'This is a message';
+  @Input() public classes = 'relative-top';
   @Input() public shape = 'help-info';
 
-  ngOnInit(): void {
-  }
+  @Input() hidden = false;
 
+  ngOnInit(): void {}
+
+  onToggleHostpot(event: Event) {
+    this.hidden = !this.hidden;
+    event.preventDefault();
+  }
 }
